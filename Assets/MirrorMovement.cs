bcb84332a3b032172f533fa8044a3e7d4c1efdb3
Orange Killer <orangekiller@oranges-mbp.mynetworksettings.com>
2023-10-30 21:56:50 -0400

    1030: body camera

--- Assets/MirrorMovement.cs
+++ Assets/MirrorMovement.cs
@@ -18,7 +18,7 @@ public class MirrorMovement : MonoBehaviour
         Vector3 localPlayer = mirror.InverseTransformPoint(playerTarget.position);
         transform.position = mirror.TransformPoint(new Vector3(localPlayer.x, localPlayer.y, -localPlayer.z));
 
-        Vector3 lookatmirror = mirror.TransformPoint(new Vector3(localPlayer.x, -localPlayer.y, -localPlayer.z));
+        Vector3 lookatmirror = mirror.TransformPoint(new Vector3(-localPlayer.x, localPlayer.y, -localPlayer.z));
         transform.LookAt(lookatmirror);
     }
 }
